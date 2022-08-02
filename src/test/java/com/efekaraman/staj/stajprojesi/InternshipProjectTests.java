2e00cd63231f72320e0f6b8bfa671a483603291b
efeka <efekaraman@hotmail.com>
2022-08-02 11:32:52 +0300

    adding tests

---
+++ src/test/java/com/efekaraman/staj/stajprojesi/InternshipProjectTests.java
@@ -0,0 +1,13 @@
+package com.efekaraman.staj.stajprojesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipProjectTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
