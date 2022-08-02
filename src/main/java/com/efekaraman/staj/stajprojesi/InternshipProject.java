c15d095bfb7fca4f62b4c33b348d0094410592ee
efeka <efekaraman@hotmail.com>
2022-08-02 11:25:23 +0300

    initial commit

---
+++ src/main/java/com/efekaraman/staj/stajprojesi/InternshipProject.java
@@ -0,0 +1,13 @@
+package com.efekaraman.staj.stajprojesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProject.class, args);
+	}
+
+}
