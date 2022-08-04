ddcf52f399e75bddcd0ec21d7b8946f24647a83b
efeka <efekaraman@hotmail.com>
2022-08-04 16:21:18 +0300

    organized imports

--- src/main/java/com/efekaraman/staj/stajprojesi/shopping_cart/CartController.java
+++ src/main/java/com/efekaraman/staj/stajprojesi/shopping_cart/CartController.java
@@ -1,11 +1,8 @@
 package com.efekaraman.staj.stajprojesi.shopping_cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
--- src/main/java/com/efekaraman/staj/stajprojesi/shopping_cart/CartRepository.java
+++ src/main/java/com/efekaraman/staj/stajprojesi/shopping_cart/CartRepository.java
@@ -1,9 +1,6 @@
 package com.efekaraman.staj.stajprojesi.shopping_cart;
 
-import com.efekaraman.staj.stajprojesi.customer.Customer;
-import com.efekaraman.staj.stajprojesi.product.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
