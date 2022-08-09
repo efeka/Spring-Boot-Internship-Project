e4389fc16a546882f6015a6c4b5ad0129844f094
efeka <efekaraman@hotmail.com>
2022-08-09 17:57:01 +0300

    refactored the frontend to improve scalability

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,46 +1,76 @@
 
-function checkControl() {
-    let laptop = document.getElementById("laptop");
-    let monitor = document.getElementById("monitor");
-    let phone = document.getElementById("phone");
+let laptop;
+let monitor;
+let phone;
+
+let cart = []
+let products = [];
+let productCount = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    setup();
+});
+
+function setup() {
+    laptop = document.getElementById("laptop");
+    monitor = document.getElementById("monitor");
+    phone = document.getElementById("phone");
+
+    products[0] = "Laptop";
+    products[1] = "Phone";
+    products[2] = "Monitor";
+    productCount = products.length;
+
+    for (let i = 0; i < productCount; i++) {
+        cart[i] = [];
+        cart[i][0] = products[i];
+        cart[i][1] = 0;
+    }
+}
+
+function addProductsToCart() {
+    if (laptop.checked === true) {
+        let index = products.indexOf("Laptop");
+        cart[index][1]++;
+    }
+    if (monitor.checked === true) {
+        let index = products.indexOf("Monitor");
+        cart[index][1]++;
+    }
+    if (phone.checked === true) {
+        let index = products.indexOf("Phone");
+        cart[index][1]++;
+    }
+
+    updateCartTable();
+}
+
+function deleteProductsFromCart() {
+    if (laptop.checked === true) {
+        let index = products.indexOf("Laptop");
+        if (cart[index][1] > 0)
+            cart[index][1]--;
+    }
+    if (monitor.checked === true) {
+        let index = products.indexOf("Monitor");
+        if (cart[index][1] > 0)
+            cart[index][1]--;
+    }
+    if (phone.checked === true) {
+        let index = products.indexOf("Phone");
+        if (cart[index][1] > 0)
+            cart[index][1]--;
+    }
+
+    updateCartTable();
+}
+
+function updateCartTable() {
     let cartItems = document.getElementById("cartItems");
-
-    if (laptop.checked == true) {
-        if(cartItems.innerHTML.includes("Laptop")) {
-            let index = cartItems.innerHTML.indexOf("Laptop x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 8));
-            count++;
-
-            cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count - 1),"Laptop x" + count);
-        }
-        else {
-            cartItems.innerHTML = cartItems.innerHTML + "<td>Laptop x1</td>"
-        }
-    }
-
-    if (monitor.checked == true) {
-        if(cartItems.innerHTML.includes("Monitor")) {
-            let index = cartItems.innerHTML.indexOf("Monitor x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 9));
-            count++;
-
-            cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count - 1),"Monitor x" + count);
-        }
-        else {
-            cartItems.innerHTML = cartItems.innerHTML + "<td style=\"text-align:center\">Monitor x1</td>"
-        }
-    }
-
-    if (phone.checked == true) {
-        if(cartItems.innerHTML.includes("Phone")) {
-            let index = cartItems.innerHTML.indexOf("Phone x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 7));
-            count++;
-
-            cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count - 1),"Phone x" + count);
-        }
-        else {
-            cartItems.innerHTML = cartItems.innerHTML + "<td>Phone x1</td>"
+    cartItems.innerHTML = "";
+    for (let i = 0; i < productCount; i++) {
+        if (cart[i][1] > 0) {
+            cartItems.innerHTML += "<td>" + cart[i][0] + " x" + cart[i][1] + "</td>";
         }
     }
 }
@@ -48,62 +78,8 @@ function checkControl() {
 function  resetCart() {
     let cartItems = document.getElementById("cartItems");
     cartItems.innerHTML = "";
-}
-
-function deleteFromCart() {
-    let laptop = document.getElementById("laptop");
-    let monitor = document.getElementById("monitor");
-    let phone = document.getElementById("phone");
-    let cartItems = document.getElementById("cartItems");
-
-    if (laptop.checked == true) {
-        if(cartItems.innerHTML.includes("Laptop")) {
-            let index = cartItems.innerHTML.indexOf("Laptop x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 8));
-            count--;
-
-            if (count == 0) {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count + 1), "");
-            }
-            else {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count + 1), "Laptop x" + count);
-            }
-        }
-    }
-
-    if (monitor.checked == true) {
-        if(cartItems.innerHTML.includes("Monitor")) {
-            let index = cartItems.innerHTML.indexOf("Monitor x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 9));
-            count--;
-
-            if (count == 0) {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count + 1), "");
-            }
-            else {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count + 1), "Monitor x" + count);
-            }
-        }
-    }
-
-    if (phone.checked == true) {
-        if(cartItems.innerHTML.includes("Phone")) {
-            let index = cartItems.innerHTML.indexOf("Phone x");
-            let count = parseInt(cartItems.innerHTML.charAt(index + 7));
-            count--;
-
-            if (count == 0) {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count + 1), "");
-            }
-            else {
-                cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count + 1), "Phone x" + count);
-            }
-        }
-    }
-
-    let innerTemp = cartItems.innerHTML;
-    if (!innerTemp.includes("Laptop") && !innerTemp.includes("Phone") && !innerTemp.includes("Monitor")) {
-        cartItems.innerHTML = "";
+    for (let i = 0; i < productCount; i++) {
+        cart[i][1] = 0;
     }
 }
 
@@ -160,3 +136,113 @@ function sendData() {
         dataType: 'json'
     });
 }
+
+/*
+function checkControl() {
+    let laptop = document.getElementById("laptop");
+    let monitor = document.getElementById("monitor");
+    let phone = document.getElementById("phone");
+    let cartItems = document.getElementById("cartItems");
+
+    if (laptop.checked === true) {
+        if(cartItems.innerHTML.includes("Laptop")) {
+            let index = cartItems.innerHTML.indexOf("Laptop x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 8));
+            count++;
+
+            cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count - 1),"Laptop x" + count);
+        }
+        else {
+            cartItems.innerHTML = cartItems.innerHTML + "<td>Laptop x1</td>"
+        }
+    }
+
+    if (monitor.checked === true) {
+        if(cartItems.innerHTML.includes("Monitor")) {
+            let index = cartItems.innerHTML.indexOf("Monitor x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 9));
+            count++;
+
+            cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count - 1),"Monitor x" + count);
+        }
+        else {
+            cartItems.innerHTML = cartItems.innerHTML + "<td style=\"text-align:center\">Monitor x1</td>"
+        }
+    }
+
+    if (phone.checked === true) {
+        if(cartItems.innerHTML.includes("Phone")) {
+            let index = cartItems.innerHTML.indexOf("Phone x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 7));
+            count++;
+
+            cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count - 1),"Phone x" + count);
+        }
+        else {
+            cartItems.innerHTML = cartItems.innerHTML + "<td>Phone x1</td>"
+        }
+    }
+}
+
+function  resetCart() {
+    let cartItems = document.getElementById("cartItems");
+    cartItems.innerHTML = "";
+}
+
+function deleteFromCart() {
+    let laptop = document.getElementById("laptop");
+    let monitor = document.getElementById("monitor");
+    let phone = document.getElementById("phone");
+    let cartItems = document.getElementById("cartItems");
+
+    if (laptop.checked === true) {
+        if(cartItems.innerHTML.includes("Laptop")) {
+            let index = cartItems.innerHTML.indexOf("Laptop x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 8));
+            count--;
+
+            if (count === 0) {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count + 1), "");
+            }
+            else {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Laptop x" + (count + 1), "Laptop x" + count);
+            }
+        }
+    }
+
+    if (monitor.checked === true) {
+        if(cartItems.innerHTML.includes("Monitor")) {
+            let index = cartItems.innerHTML.indexOf("Monitor x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 9));
+            count--;
+
+            if (count === 0) {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count + 1), "");
+            }
+            else {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Monitor x" + (count + 1), "Monitor x" + count);
+            }
+        }
+    }
+
+    if (phone.checked === true) {
+        if(cartItems.innerHTML.includes("Phone")) {
+            let index = cartItems.innerHTML.indexOf("Phone x");
+            let count = parseInt(cartItems.innerHTML.charAt(index + 7));
+            count--;
+
+            if (count === 0) {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count + 1), "");
+            }
+            else {
+                cartItems.innerHTML = cartItems.innerHTML.replace("Phone x" + (count + 1), "Phone x" + count);
+            }
+        }
+    }
+
+    let innerTemp = cartItems.innerHTML;
+    if (!innerTemp.includes("Laptop") && !innerTemp.includes("Phone") && !innerTemp.includes("Monitor")) {
+        cartItems.innerHTML = "";
+    }
+}
+*/
